dc72b8ea25e90a873c1aec753071f9c98e303f26
Ammoh-Moringa <kiprotichamos674@gmail.com>
2021-09-08 13:48:45 +0300

    Create Models

--- VotaSec/settings.py
+++ VotaSec/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'votapp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'VotaSec.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vota',
+        'USER': 'jose',
+    'PASSWORD':'joseotis45',
     }
 }
 
@@ -123,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'votapp.User'
--- votapp/models.py
+++ votapp/models.py
@@ -23,19 +23,95 @@ class teacher(models.Model):
         return self.user.username
 
 
+lessons = [('English', 'English'),
+           ('Kiswahili', 'Kiswahili'),
+           ('Maths', 'Maths'),
+           ('Chemistry', 'Chemistry'),
+           ('Biology', 'Biology'),
+           ('Physics','Physics'),
+           ('C.R.E', 'C.R.E'),
+           ('History and Government', 'History and Government'),
+           ('Geography','Geography'),
+           ('Business Studies','Business Studies'),
+           ('Agriculture','Agriculture')]
+
+grades = [('A','A'),
+        ('A-','A-'),
+        ('B+','B+'),
+        ('B','B'),
+        ('B-','B-'),
+        ('C+','C+'),
+        ('C','C'),
+        ('C-','C-'),
+        ('D+','D+'),
+        ('D','D'),
+        ('D-','D-'),
+        ('E','E'),
+        ]
+
+Exams = [('Exam 1','Exam 1'),
+        ('Exam 2', 'Exam 2'),
+        ('End term','End term'),
+        
+        ]
+
+remark = [('Excellent','Excellent'),
+        ('Very Good', 'Very Good'),
+        ('Good','Good'),
+        ('Average','Average'),
+        ('Put more effort', 'Put more effort'),
+        ('Poor','Poor'),
+        
+        ]
+
+        
+        
+
+           
+
+class Subject(models.Model) :
+    subject=models.CharField(max_length=1000,choices=lessons, default='none')
+    marks=models.IntegerField(null=True, blank=True)
+    grade=models.CharField(max_length=100,choices=grades, default='E')
+    examinations = models.CharField(max_length=100, choices=Exams,default='0')
+    points=models.IntegerField(null=True, blank=True)
+    position= models.IntegerField(null=True, blank=True)
+    remarks = models.CharField(max_length=1000, choices=remark, default='none')
+    initials= models.CharField(max_length=100)
+
+
 classes = [('form-one', 'form-one'),
            ('form-two', 'form-two'),
            ('form-three', 'form-three'),
            ('form-four', 'form-four')]
+
+grades = [('A','A'),
+        ('A-','A-'),
+        ('B+','B+'),
+        ('B','B'),
+        ('B-','B-'),
+        ('C+','C+'),
+        ('C','C'),
+        ('C-','C-'),
+        ('D+','D+'),
+        ('D','D'),
+        ('D-','D-'),
+        ('E','E'),
+        ]
+
            
 class StudentExtra(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    feeball=models.PositiveIntegerField(null=True)
-    nextfees=models.PositiveIntegerField(null=True)
+    feeball=models.PositiveIntegerField(null=True, blank=True)
+    nextfees=models.PositiveIntegerField(null=True, blank=True)
     form= models.CharField(max_length=10,choices=classes,default='form-one')
-    meangrade=models.
-    totalmaks=
-    meanmark=
+    meangrade=models.CharField(max_length=100,choices=grades, default='E')
+    totalmaks=models.IntegerField(null=True, blank=True)
+    meanmark=models.IntegerField(null=True, blank=True)
+    subjects=models.ForeignKey(Subject,on_delete=models.CASCADE, related_name='sub')
+    admno=models.IntegerField(null=True, blank=True)
+    hse=models.CharField(max_length=1000)
+
    
     @property
     def get_name(self):
@@ -45,3 +121,5 @@ class StudentExtra(models.Model):
         return self.user.id
     def __str__(self):
         return self.user.first_name
+
+
